0f0cb4657c5b2e1e0c07dc31b5ce15e5679efac5
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-24 09:16:38 +0200

    function to paint the list of clubs

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import '../styles/App.scss';
+import { useState } from 'react';
+import initialClubs from '../data/clubs.json';
 
 function App() {
-	return <div></div>;
+	const [clubsList, setClubsList] = useState(initialClubs);
+
+	const htmlClubsList = () => {
+		return clubsList.map((oneClub, index) => {
+			return (
+				<li key={index} className="clubsList__item">
+					<h2>{`#${index}: ${oneClub.name}`}</h2>
+					<p>{`Abierto entre semana: ${
+						oneClub.openOnWeekdays ? 'Sí' : 'No'
+					}`}</p>
+					<p>{`Abierto el fin de semana: ${
+						oneClub.openOnWeekend ? 'Sí' : 'No'
+					}`}</p>
+				</li>
+			);
+		});
+	};
+	return (
+		<div>
+			<header className="header">
+				<h1 className="header__title">Mis clubs</h1>
+			</header>
+			<main className="main">
+				<ul className="clubsList">{htmlClubsList()}</ul>
+			</main>
+		</div>
+	);
 }
 
 export default App;
