9c4d8b6089b6046e560a3301211dad4dc1c09d16
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-24 10:56:33 +0200

    add new club function

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,21 @@ import initialClubs from '../data/clubs.json';
 function App() {
 	const [clubsList, setClubsList] = useState(initialClubs);
 
+	const [newName, setNewName] = useState('');
+	const [newWeek, setNewWeek] = useState(false);
+	const [newWeekend, setNewWeekend] = useState(false);
+
+	const handleAddNewName = (event) => {
+		setNewName(event.currentTarget.value);
+	};
+
+	const handleAddNewWeek = (event) => {
+		setNewWeek(event.currentTarget.checked);
+	};
+
+	const handleAddNewWeekend = (event) => {
+		setNewWeekend(event.currentTarget.checked);
+	};
 	const htmlClubsList = () => {
 		return clubsList.map((oneClub, index) => {
 			return (
@@ -21,6 +36,21 @@ function App() {
 		});
 	};
 
+	const handleClick = (event) => {
+		event.preventDefault();
+		const newClub = {
+			name: newName,
+			openOnWeekdays: newWeek,
+			openOnWeekend: newWeekend,
+		};
+
+		setClubsList([...clubsList, newClub]);
+
+		setNewName('');
+		setNewWeek(false);
+		setNewWeekend(false);
+	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 	};
@@ -33,7 +63,48 @@ function App() {
 			<main className="main">
 				<ul className="clubsList">{htmlClubsList()}</ul>
 
-				<form onSubmit={handleSubmit}></form>
+				<form className="form" onSubmit={handleSubmit}>
+					<header className="header">
+						<h2 className="header__title">Añadir un nuevo club</h2>
+					</header>
+					<label htmlFor="name">
+						Nombre del club
+						<input
+							type="text"
+							name="name"
+							id="name"
+							onChange={handleAddNewName}
+							value={newName}
+						/>
+					</label>
+
+					<label htmlFor="week">
+						¿Abre entre semana?
+						<input
+							type="checkbox"
+							name="week"
+							id="week"
+							onChange={handleAddNewWeek}
+							checked={newWeek}
+						/>
+					</label>
+					<label htmlFor="weekend">
+						¿Abre los fines de semana?
+						<input
+							type="checkbox"
+							name="weekend"
+							id="weekend"
+							onChange={handleAddNewWeekend}
+							checked={newWeekend}
+						/>
+					</label>
+					<input
+						onClick={handleClick}
+						type="submit"
+						value="Añadir un nuevo club"
+						className="form__btn"
+					/>
+				</form>
 			</main>
 		</div>
 	);
